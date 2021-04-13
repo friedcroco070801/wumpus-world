a6dc6baeb73639d47f8739fd846336ae81dc876a
friedcroco070801 <trung.nguyencasau@gmail.com>
2021-04-13 14:26:28 +0700

    Update game color palette

---
+++ lib/gamescreen/colorpallete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameColor {
+  static final Color BLACK = Color.fromRGBO(0xFF, 0xFF, 0xFF, 1);
+  static final Color BLUE = Color.fromRGBO(0x01, 0x29, 0x5F, 1);
+  static final Color TEAL = Color.fromRGBO(0x43, 0x7F, 0x97, 1);
+  static final Color GREEN = Color.fromRGBO(0x84, 0x93, 0x24, 1);
+  static final Color YELLOW = Color.fromRGBO(0xFF, 0xB5, 0x0F, 1);
+  static final Color RED = Color.fromRGBO(0xFD, 0x15, 0x1B, 1);
+}
